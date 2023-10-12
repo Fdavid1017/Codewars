59d490e64f627afadfa9ead6a6fc62ebe73018a6
Fábry Dávid <fdavid1017@gmail.com>
2023-10-12 11:13:18 +0200

    something

--- TestSolutions/Program.cs
+++ TestSolutions/Program.cs
@@ -2,63 +2,20 @@
 
 class TestClass
 {
+    public enum Direction { Up = 8, Down = 2, Left = 4, Right = 6 }
+
     static void Main(string[] args)
     {
-        string[,] test = new string[,]
-        {
-           {"R", "Y", "Y", "Y", "R", "-",},
-           {"Y", "-", "-", "-", "-", "-",},
-           {"R", "Y", "R", "Y", "R", "Y",},
-           {"Y", "R", "-", "-", "-", "-",},
-           {"R", "R", "R", "Y", "-", "-",},
-           {"R", "-", "-", "-", "-", "-",},
-           {"Y", "Y", "R", "-", "-", "-", },
-        };
+        PriorityQueue<Direction, DateTime> queue = new PriorityQueue<Direction, DateTime>();
 
-        int column = 0;
-        int row = 4;
+        queue.Enqueue(Direction.Up, DateTime.Now);
+        queue.Enqueue(Direction.Down, DateTime.Now);
+        queue.Enqueue(Direction.Left, DateTime.Now);
 
-        //for (int i = 0; i < test.GetLength(0); i++)
-        //{
-        //    for (int j = 0; j < test.GetLength(1); j++)
-        //    {
-        //        test[i, j] = "-";
-        //    }
-        //}
+        queue.Dequeue();
 
+        
 
-        for (int i = column, j = row; i < test.GetLength(0) && j < test.GetLength(1); i++, j++)
-        {
-            test[i, j] = "1";
-        }
-
-        // Values down
-        for (int i = column - 1, j = row - 1; i >= 0 && j >= 0; i--, j--)
-        {
-            test[i, j] = "2";
-        }
-
-        for (int i = column, j = row; i < test.GetLength(0) && j >= 0; i++, j--)
-        {
-            test[i, j] = "3";
-        }
-
-        // Values down
-        for (int i = column - 1, j = row + 1; j >= 0 && j < test.GetLength(1) && i < test.GetLength(0) && i >= 0; i--, j++)
-        {
-            test[i, j] = "4";
-        }
-
-        //test[column, row] = "0";
-
-        for (int i = 0; i < test.GetLength(0); i++)
-        {
-            for (int j = 0; j < test.GetLength(1); j++)
-            {
-                Console.Write(test[i, j] + " ");
-            }
-
-            Console.WriteLine();
-        }
+        Console.WriteLine("END");
     }
 }
